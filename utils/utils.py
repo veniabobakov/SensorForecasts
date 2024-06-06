7752905d2a153b0e4483a5dd04f7fa6b6f0cadff
user <veniabobakov97@gmail.com>
2024-06-06 18:39:54 +0500

    [2] add update of plot for error of learning

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,19 @@
 from pathlib import Path
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
 
 
 def get_project_root() -> Path:
     return Path(__file__).parent.parent
+
+
+def update_plot(epoch, train_losses):
+    clear_output(wait=False)
+    plt.figure(figsize=(10, 5))
+    plt.plot(train_losses, label='Train Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.title(f'Epoch {epoch + 1}')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
